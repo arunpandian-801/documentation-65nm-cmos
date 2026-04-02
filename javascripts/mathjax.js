ec71ef53432bbe6d6c396ec85a06d92fb7ac2d17
Arunpandian A <arun.pandian801@outlook.com>
2026-04-03 00:33:17 +0530

    Deployed caa4664 with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    // Useful packages for analog IC design equations
+    packages: { "[+]": ["boldsymbol"] }
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
